16126f1724282d5a0d04960a2dff180cc394c5a9
iamrahulraj17 <nowluckyabhi2016@gmail.com>
2024-10-10 13:46:44 +0530

    Code completed

--- 11.Calculator.c
+++ 11.Calculator.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,o;
+    int a,b;
+    char o;
     printf("Enter the expression: ");
     scnaf("%d%d%d",&a,&o,&b);
-
+    switch (o)
+    {
+        case '+':
+        printf("%d + %d = %d",a,b,a+b);
+        break;
+        case '-':
+        printf("%d - %d = %d",a,b,a-b);
+        break;
+        case '*':
+        printf("%d * %d = %d",a,b,a*b);
+        break;
+        case '/':
+        printf("%d / %d = %d",a,b,a/b);
+        break;
+        default:
+        printf("Invalid Expression!!!");
+    }
     return 0;
 }
